2b54dfeaa2b958d11603330ad46e6186ce7041ad
Ivan Malyshev <ivan.malyshevv@gmail.com>
2026-05-31 14:51:51 +0300

    Fix Dockerfile and update settings for Kittygram

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', 'ivan-kittygram.ru']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', 'ivan-kittygram.ru', 'ivan-kittygram.online', '158.160.233.230']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
